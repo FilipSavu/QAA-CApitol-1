8481d6e0f2c87eda02654aa6651b7d9f66926fcd
savu filip <savufilip@gmail.com>
2025-03-10 11:47:23 +0200

    Create capitolul6exempluextins.js

---
+++ capitolul6exempluextins.js
@@ -0,0 +1,21 @@
+// Obiect care reprezinta un raft de carti
+const raftcarti = {
+    authors: [], // Lista autorilor de pe raft
+    
+    // Metoda care returneaza lista de autori
+    getAuthors: function() {
+        return this.authors; // 'this' se refera la obiectul 'bookshelf'
+    },
+    
+    // Metoda care adauga un autor in lista
+    addAuthor: function(authorName) {
+        this.authors.push(authorName); // 'this' se refera la obiectul 'bookshelf'
+    }
+};
+
+// Adaugam autori
+raftcarti.addAuthor("scriitor1");
+raftcarti.addAuthor("scriitor2");
+
+// Afisam lista de autori
+console.log(raftcarti.getAuthors()); // Afiseaza ["scriitor1", "scriitor2"]
